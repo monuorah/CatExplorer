dc0ac860d2718e25e425744561c98793cf98a06e
Muna Onuorah <jonuorah@ymail.com>
2025-10-23 16:07:29 -0500

    Initial Commit

---
+++ CatExplorer/CatExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatExplorerApp.swift
+//  CatExplorer
+//
+//  Created by Muna Onuorah on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct CatExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatExplorer
+//
+//  Created by Muna Onuorah on 10/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
